d3a49449d6bef71e2d05c363b0f39fb5746c1ac1
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2025-09-25 17:42:49 -0600

    9/25

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log('Hello World')
+/*
+Exercise 2: Add strings to the array
+
+1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
+
+Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
+
+Complete Exercise 2 in the space below:
+*/
+
+const foods = ['pizza', 'cheeseburger']
+
+console.log('Exercise 2 result:', foods);
+
+/*
+Exercise 3: Insert at the beginning
+
+1) Insert the string 'taco' at the beginning of the `foods` array.
+
+Complete Exercise 3 in the space below:
+*/
+
+foods.unshift('taco')
+
+console.log('Exercise 3 result:', foods);
