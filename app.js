d102963afff5330eea9efbb9b9d029c8e7827457
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2025-09-25 17:51:13 -0600

    9/25 v2

--- app.js
+++ app.js
@@ -24,3 +24,31 @@ Complete Exercise 3 in the space below:
 foods.unshift('taco')
 
 console.log('Exercise 3 result:', foods);
+
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+
+const favFood = foods[1]
+
+console.log('Exercise 4 result:', favFood);
+
+/*
+Exercise 5: Insert an element between two others
+
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+
+Complete Exercise 5 in the space below:
+*/
+
+foods.splice(2, 0, 'tofu')
+
+console.log('Exercise 5 result:', foods);
+
